84b616e9df7049fb0a8798628afad6581281ba20
Kasia Tunska <k.j.tunska@gmail.com>
2022-03-23 16:43:42 +0100

    modul18

---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class TasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksApplication.class, args);
+    }
+}
+    }
+}
